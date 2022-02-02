33bd6849b10a8af99b96d82fb9aa33ebc975945e
Daniel Tostes <ohtosttnz@gmail.com>
2022-02-02 16:34:21 -0300

    Adiciona or.js e and.js

---
+++ 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/operadores-logicos/or.js
@@ -0,0 +1,8 @@
+let weekDay = 'quarta-feira';
+
+if (weekDay === 'sabado' || weekDay === 'domingo') {
+  console.log("FINALMENTE, descanso merecido UwU")
+}
+else {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
