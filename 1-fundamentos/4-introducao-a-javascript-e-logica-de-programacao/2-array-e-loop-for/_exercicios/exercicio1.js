38cdd1188ce8c632c435615a9a9b1536f13514f9
Daniel Tostes <ohtosttnz@gmail.com>
2022-02-03 15:45:36 -0300

    Finaliza exercicio 1

---
+++ 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/2-array-e-loop-for/_exercicios/exercicio1.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercicio 1 - Percorrer o array e imprimir todos os valores
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+}
