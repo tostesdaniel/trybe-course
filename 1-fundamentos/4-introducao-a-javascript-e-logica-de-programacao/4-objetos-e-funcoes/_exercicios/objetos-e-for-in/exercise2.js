cecccbbf0b62e922a8434076b97e8e823e9f756a
Daniel Tostes <ohtosttnz@gmail.com>
2022-02-07 18:29:33 -0300

    Finaliza exercicio 2

---
+++ 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4-objetos-e-funcoes/_exercicios/objetos-e-for-in/exercise2.js
@@ -0,0 +1,10 @@
+// Insira no objeto a propriedade 'recorrente' com o valor 'Sim', e imprima o objeto
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+info['recorrente'] = 'Sim';
+
+console.log(info);
