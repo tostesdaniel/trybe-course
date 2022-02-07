1dd15254b9373b9808132d85f89dbbe73ba1f3ce
Daniel Tostes <ohtosttnz@gmail.com>
2022-02-07 19:03:07 -0300

    Finaliza exercicio 6

---
+++ 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4-objetos-e-funcoes/_exercicios/objetos-e-for-in/exercise6.js
@@ -0,0 +1,16 @@
+// Acesse as chaves 'nome', 'sobrenome', 'titulo' e faça um 'console.log'
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log('O livro favorito de ' + leitor['nome'] + ' ' + leitor['sobrenome'] + ' se chama "' + leitor['livrosFavoritos'][0]['titulo'] + '".');
