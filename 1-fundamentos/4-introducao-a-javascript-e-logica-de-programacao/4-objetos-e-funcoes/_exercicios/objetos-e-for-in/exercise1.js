da4b36442cbb0055b48dc06808c94832ec242ca1
Daniel Tostes <ohtosttnz@gmail.com>
2022-02-07 18:26:12 -0300

    Finaliza exercicio 1

---
+++ 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4-objetos-e-funcoes/_exercicios/objetos-e-for-in/exercise1.js
@@ -0,0 +1,8 @@
+// Imprima uma mensagem de boas vindas para a personagem
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+console.log('Bem vinda, ' + info['personagem']);
