154d2114c7fb606fc0ff547670af2ff18e4b39e9
Daniel Tostes <ohtosttnz@gmail.com>
2022-02-07 18:52:37 -0300

    Finaliza exercicio 5

---
+++ 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4-objetos-e-funcoes/_exercicios/objetos-e-for-in/exercise5.js
@@ -0,0 +1,23 @@
+// Defina um segundo objeto com a mesma estrutura do primeiro, porem com diferentes valores. Depois, imprima os valores de todas as chaves dos objetos juntos
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info, info2) {
+  if (info[key] === 'Sim' && info2[key] === 'Sim') {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
+}
