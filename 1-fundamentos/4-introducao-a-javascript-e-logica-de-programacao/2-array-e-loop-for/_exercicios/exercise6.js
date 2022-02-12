7ee740f38a659ceff5ab9be8990a12cadf913fac
Daniel Tostes <ohtosttnz@gmail.com>
2022-02-12 16:21:53 -0300

    Adiciona 'Exercicio 6 - ' na linha 3

--- 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/2-array-e-loop-for/_exercicios/exercise6.js
+++ 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/2-array-e-loop-for/_exercicios/exercise6.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Descobrir quantos valores ímpares tem no array e imprimir o resultado. Caso não haja números ímpares, imprimir "nenhum valor ímpar encontrado"
+// Exercicio 6 - Descobrir quantos valores ímpares tem no array e imprimir o resultado. Caso não haja números ímpares, imprimir "nenhum valor ímpar encontrado"
 
 let oddNumbers = 0;
 for (const number of numbers) {
