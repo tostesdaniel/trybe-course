4bc007bdbbeb4f350e4a3c97196285edd48626d5
Daniel Tostes <ohtosttnz@gmail.com>
2022-02-12 16:39:55 -0300

    Finaliza exercicio 8

---
+++ 1-fundamentos/4-introducao-a-javascript-e-logica-de-programacao/2-array-e-loop-for/_exercicios/exercise8.js
@@ -0,0 +1,7 @@
+// Exercicio 8 - Utilize o for para criar um array que vá de 1 a 25 e imprima este array
+
+let numbers = [];
+for (let currentNumber = 0; currentNumber <= 25; currentNumber += 1) {
+    numbers.push(currentNumber);
+}
+console.log(numbers);
