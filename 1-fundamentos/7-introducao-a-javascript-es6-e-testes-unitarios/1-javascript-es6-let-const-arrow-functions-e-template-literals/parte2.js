587d55aa7c77e71118a85cbd99b8d53a757ddde4
Daniel <ohtosttnz@gmail.com>
2022-03-04 18:23:10 -0300

    Refatora função 1

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
@@ -1,3 +1,5 @@
-// Criada uma função que recebe um número e retorna seu fatorial, de forma recursiva
+// Exercicio 1 - Criada uma função que recebe um número e retorna seu fatorial, de forma recursiva
 const calcFactorial = (n) => n === 1 || n === 0 ? 1 : n * calcFactorial(n - 1);
 console.log(calcFactorial(3));
+
+// Exercicio 2 - Criada uma função que recebe uma string e retorna sua maior palavra
