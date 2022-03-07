fd2c3c7d12d0b7065e1fa0afa61a088edd8a701b
Daniel <ohtosttnz@gmail.com>
2022-03-07 01:08:43 -0300

    Conclui o exercicio 2

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
@@ -1,5 +1,21 @@
 // Exercicio 1 - Criada uma função que recebe um número e retorna seu fatorial, de forma recursiva
 const calcFactorial = (n) => n === 1 || n === 0 ? 1 : n * calcFactorial(n - 1);
-console.log(calcFactorial(3));
+console.log(calcFactorial(4));
 
 // Exercicio 2 - Criada uma função que recebe uma string e retorna sua maior palavra
+const findLongestWord = phrase => {
+  let wordsArray = phrase.split(' ');
+  let longestWordLength = 0;
+  let longestWord = '';
+
+  for (const word of wordsArray) {
+    if (word.length > longestWordLength) {
+      longestWordLength = word.lenght;
+      longestWord = word;
+    }
+  }
+
+  return longestWord;
+}
+
+console.log(findLongestWord('JavaScript é legal mas as vezes pode ser complicado'));
