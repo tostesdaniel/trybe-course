564c61075b480d37d5b832aa1061e3ced4334f11
Daniel <ohtosttnz@gmail.com>
2022-03-09 19:00:15 -0300

    Conclui exercicio 1

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/_exercicios/agora-a-pratica/exercise1.js
@@ -0,0 +1,18 @@
+// Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com . 
+
+const genEmail = (fullName) => {
+  const email = fullName.toLowerCase().split(' ').join('_');
+  const fullEmail = { fullName, email: `${email}@trybe.com`};
+  return fullEmail;
+}
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(genEmail));
