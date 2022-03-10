0503e04dd1e807f608669b5692948b50d79e6944
Daniel <ohtosttnz@gmail.com>
2022-03-10 19:15:44 -0300

    Conclui "Para Fixar - 2"

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/2-higher-order-functions-foreach-find-some-every-sort/conteudos/Array.sort/exercise2.js
@@ -0,0 +1,13 @@
+// Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+people.sort((a, b) => b.age - a.age);
+
+console.log(people);
