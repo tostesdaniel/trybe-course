e47515b231efea9fd01aab0c8c0d21760056e78d
Daniel <ohtosttnz@gmail.com>
2022-03-19 19:40:33 -0300

    Altera exercicio 1

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/_exercicios/agora-a-pratica/exercise1.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/_exercicios/agora-a-pratica/exercise1.js
@@ -8,9 +8,9 @@ const genEmail = (fullName) => {
 
 const newEmployees = (callback) => {
   const employees = {
-    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: genEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: genEmail('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: genEmail('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
