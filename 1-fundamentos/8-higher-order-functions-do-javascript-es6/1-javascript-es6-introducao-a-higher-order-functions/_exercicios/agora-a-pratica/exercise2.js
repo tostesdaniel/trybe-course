83b51526949706942db61cc3bb66ab53dd9d1ce8
Daniel <ohtosttnz@gmail.com>
2022-03-19 20:00:22 -0300

    Adiciona enunciado ao exercicio 2

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/_exercicios/agora-a-pratica/exercise2.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/_exercicios/agora-a-pratica/exercise2.js
@@ -1,3 +1,5 @@
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou"). 
+
 function raffle() {
   const selNumber = Math.floor(Math.random() * (5 - 1 + 1) + 1);
   return selNumber;
