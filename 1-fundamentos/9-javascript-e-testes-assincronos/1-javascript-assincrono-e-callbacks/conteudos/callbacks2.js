7da8aa97ed2abfb0d51a734972829f3dd5e15525
Daniel <ohtosttnz@gmail.com>
2022-03-21 16:14:00 -0300

    Conclui "Para Fixar - 2" de Callbacks

---
+++ 1-fundamentos/9-javascript-e-testes-assincronos/1-javascript-assincrono-e-callbacks/conteudos/callbacks2.js
@@ -0,0 +1,28 @@
+/* Passe, como parâmetro e como retorno, uma callback para a função getUser .
+No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono e imprime dados de uma pessoa depois de um certo tempo. Complete a função getUser de forma que ela receba uma callback como parâmetro e a retorne para que possa realizar as operações abaixo sobre a pessoa:
+
+    Insira uma callback como parâmetro da função getUser ;
+    Retorne a callback passada como parâmetro na função getUser ; */
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    console.log(callback(user));
+  }, delay());
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+// Obs.: Analise o comportamento assíncrono da função getUser ao chamar getUser(userFullName) seguido de getUser(userNationality) . Tem hora que o nome da pessoa é impresso antes e tem hora que a nacionalidade da pessoa é impressa antes!
