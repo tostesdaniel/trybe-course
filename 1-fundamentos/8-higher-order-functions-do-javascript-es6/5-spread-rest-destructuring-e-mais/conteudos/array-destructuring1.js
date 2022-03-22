32fa31394e7424fe90d3fcb44dec448cc341d191
Daniel <ohtosttnz@gmail.com>
2022-03-22 12:43:18 -0300

    Conclui "Para Fixar - 1" de Array Destructuring

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/5-spread-rest-destructuring-e-mais/conteudos/array-destructuring1.js
@@ -0,0 +1,9 @@
+// Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação.
+
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+const  [greetMsg, greeting] = saudacoes;
+greeting(greetMsg);
