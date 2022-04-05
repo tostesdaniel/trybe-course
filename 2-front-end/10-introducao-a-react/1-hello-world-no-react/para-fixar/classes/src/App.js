ddc4549876e5ac7461a8863cee4249cf5ee2f3ad
Daniel <ohtosttnz@gmail.com>
2022-04-05 11:55:38 -0300

    Conclui "Para Fixar" de "Classes e React"

---
+++ 2-front-end/10-introducao-a-react/1-hello-world-no-react/para-fixar/classes/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Component';
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ 2-front-end/10-introducao-a-react/1-hello-world-no-react/para-fixar/classes/src/Component.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Header extends Component {
+  render () {
+    return <div>
+      <h1>
+        <p>This is a brief test</p>
+      </h1>
+    </div>
+  }
+}
+
+export default Header;
