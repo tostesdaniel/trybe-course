f9f859fba43143dfe4b18b6af198e8a0392cf909
Daniel <ohtosttnz@gmail.com>
2022-04-07 15:58:49 -0300

    Conclui fixação de this e bind no constructor

--- 2-front-end/11-estado-eventos-e-formularios-com-react/1-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ 2-front-end/11-estado-eventos-e-formularios-com-react/1-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -1,25 +1,35 @@
+// 1 - Crie uma aplicação React com npx create-react-app fancy-buttons . Altere o componente App.js para que seja um componente de classe e contenha um botão associado a um evento que imprime um texto qualquer via console.log(). 
+
 import React from 'react';
 import './App.css';
 
-function handleFirstBtnClick() {
-  console.log('This is the first button.');
-}
-
-function handleSecondBtnClick() {
-  console.log('This is the second button.');
-}
-
-function handleThirdBtnClick() {
-  console.log('This is the third button.');
-}
-
 class App extends React.Component {
+// 4 - Garanta acesso ao objeto this na função que você declarou.
+  constructor() {
+    super()
+    this.handleFirstBtnClick = this.handleFirstBtnClick.bind(this)
+    this.handleSecondBtnClick = this.handleSecondBtnClick.bind(this)
+    this.handleThirdBtnClick = this.handleThirdBtnClick.bind(this)
+  }
+// 3 - Declare dentro da classe do seu componente dos exercícios de fixação acima a função que lida com o evento que antes era lidado por uma função do lado de fora da classe!
+  handleFirstBtnClick() {
+    console.log('This is the first button.');
+  }
+  
+  handleSecondBtnClick() {
+    console.log('This is the second button.');
+  }
+  
+  handleThirdBtnClick() {
+    console.log('This is the third button.');
+  }
+// 2 - Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log().
   render() {
     return (
       <>
-        <button onClick={handleFirstBtnClick}>(1)</button>
-        <button onClick={handleSecondBtnClick}>(2)</button>
-        <button onClick={handleThirdBtnClick}>(3)</button>
+        <button onClick={this.handleFirstBtnClick}>(1)</button>
+        <button onClick={this.handleSecondBtnClick}>(2)</button>
+        <button onClick={this.handleThirdBtnClick}>(3)</button>
       </>
     );
   }
