8d508158006326fd7c3f995a4e28af864da2df29
Daniel <ohtosttnz@gmail.com>
2022-04-07 20:10:16 -0300

    Conclui fixação de states e events

--- 2-front-end/11-estado-eventos-e-formularios-com-react/1-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ 2-front-end/11-estado-eventos-e-formularios-com-react/1-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -4,32 +4,61 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
 // 4 - Garanta acesso ao objeto this na função que você declarou.
   constructor() {
     super()
     this.handleFirstBtnClick = this.handleFirstBtnClick.bind(this)
     this.handleSecondBtnClick = this.handleSecondBtnClick.bind(this)
     this.handleThirdBtnClick = this.handleThirdBtnClick.bind(this)
+
+// 5 - Agora você vai converter sua aplicação para uma que conta o número de cliques dado em cada botão! Primeiramente, defina um estado inicial para a contagem de cada botão.
+    this.state = {
+      firstBtnClicks: 0,
+      secondBtnClicks: 0,
+      thirdBtnClicks: 0,
+    };
   }
+
 // 3 - Declare dentro da classe do seu componente dos exercícios de fixação acima a função que lida com o evento que antes era lidado por uma função do lado de fora da classe!
+
+// 6 - Agora, quando um botão for clicado, altere de forma assíncrona o estado deste botão de zero para um.
+
+// 7 - Por fim, baseie-se no estado anterior ao atual para incrementar a contagem de cliques cada vez que um botão for clicado!
   handleFirstBtnClick() {
-    console.log('This is the first button.');
+    // console.log('This is the first button.');
+    this.setState((state, _props) => ({
+
+      // firstBtnClicks: 1,
+      firstBtnClicks: state.firstBtnClicks + 1
+    }));
   }
-  
+
   handleSecondBtnClick() {
-    console.log('This is the second button.');
+    // console.log('This is the second button.');
+    this.setState((state, _props) => ({
+
+      // secondBtnClicks: 1,
+      secondBtnClicks: state.secondBtnClicks + 1
+    }));
   }
-  
+
   handleThirdBtnClick() {
-    console.log('This is the third button.');
+    // console.log('This is the third button.');
+    this.setState((state, _props) => ({
+
+      // thirdBtnClicks: 1,
+      thirdBtnClicks: state.thirdBtnClicks + 1
+    }));
   }
+
 // 2 - Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log().
   render() {
     return (
       <>
-        <button onClick={this.handleFirstBtnClick}>(1)</button>
-        <button onClick={this.handleSecondBtnClick}>(2)</button>
-        <button onClick={this.handleThirdBtnClick}>(3)</button>
+        <button onClick={ this.handleFirstBtnClick }>{ this.state.firstBtnClicks }</button>
+        <button onClick={ this.handleSecondBtnClick }>{ this.state.secondBtnClicks }</button>
+        <button onClick={ this.handleThirdBtnClick }>{ this.state.thirdBtnClicks }</button>
       </>
     );
   }
