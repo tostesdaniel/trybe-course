23f2c95afb75415ee762e28e14012a6d6e26dfa9
Daniel Tostes <ohtosttnz@gmail.com>
2022-05-07 14:45:48 -0300

    Exercicio 1.3 ✅

--- 2-front-end/14-testes-automatizados-com-RTL/2-mocks-e-inputs/mocks-e-inputs/src/tests/service.test.js
+++ 2-front-end/14-testes-automatizados-com-RTL/2-mocks-e-inputs/mocks-e-inputs/src/tests/service.test.js
@@ -1,7 +1,7 @@
 const services = require('./service')
 
-describe('Teste de Mocks e Inputs', () => {
-  it('Testa a função getRandomInt', () => {
+describe('Teste de Mocks', () => {
+  it('1 - Testa a função getRandomInt', () => {
     services.getRandomInt = jest.fn().mockReturnValue(10);
 
     expect(services.getRandomInt()).toBe(10);
@@ -9,12 +9,36 @@ describe('Teste de Mocks e Inputs', () => {
     expect(services.getRandomInt).toHaveBeenCalledTimes(1);
   });
 
-  it('Testa', () => {
+  it('2 - Testa implementação mockada da função', () => {
     services.getRandomInt = jest.fn().mockImplementation((x, y) => x / y);
 
     expect(services.getRandomInt(100, 10)).toBe(10);
     expect(services.getRandomInt).toHaveBeenCalled();
     expect(services.getRandomInt).toHaveBeenCalledTimes(1);
     expect(services.getRandomInt).toHaveBeenCalledWith(100, 10);
-  })
+  });
+});
+
+describe('3 - Testa implementações', () => {
+  it('Testa implementação com uma multiplicação de três números', () => {
+    services.getRandomInt = jest.fn()
+    .mockImplementation((x, y, z) => x * y * z);
+
+    expect(services.getRandomInt(1, 2, 3)).toBe(6);
+    expect(services.getRandomInt).toHaveBeenCalled();
+    expect(services.getRandomInt).toHaveBeenCalledTimes(1);
+    expect(services.getRandomInt).toHaveBeenCalledWith(1, 2, 3);
+  });
+
+  it('Testa implementação com o dobro do valor de um número', () => {
+    services.getRandomInt.mockReset();
+    expect(services.getRandomInt).toHaveBeenCalledTimes(0);
+    
+    services.getRandomInt = jest.fn().mockImplementation((n) => 2 * n);
+
+    expect(services.getRandomInt(10)).toBe(20);
+    expect(services.getRandomInt).toHaveBeenCalled();
+    expect(services.getRandomInt).toHaveBeenCalledTimes(1);
+    expect(services.getRandomInt).toHaveBeenCalledWith(10);
+  });
 });
