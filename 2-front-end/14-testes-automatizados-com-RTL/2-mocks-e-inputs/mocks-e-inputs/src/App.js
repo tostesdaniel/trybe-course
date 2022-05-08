74fa254a5aa4d11fe2c12073b94843b3743b7fe8
Daniel Tostes <ohtosttnz@gmail.com>
2022-05-08 15:07:49 -0300

    Cria um app que renderiza fotos de cachorro

--- 2-front-end/14-testes-automatizados-com-RTL/2-mocks-e-inputs/mocks-e-inputs/src/App.js
+++ 2-front-end/14-testes-automatizados-com-RTL/2-mocks-e-inputs/mocks-e-inputs/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      dogImage: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const dogImage = this.fetchDogImage();
+    this.setState({
+      dogImage,
+    });
+  }
+
+  fetchDogImage = async () => {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    const response = await fetch(url);
+    const { message } = await response.json();
+    console.log(message);
+    this.setState({
+      dogImage: message,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <img src={this.state.dogImage} alt="Just a dog" />
+      </div>
+    );
+  }
 }
 
 export default App;
