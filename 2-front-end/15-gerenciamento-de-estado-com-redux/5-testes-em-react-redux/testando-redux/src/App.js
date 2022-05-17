17d46f83e9ecbff80d67e5b2eb2c939abcdcf88f
Daniel Tostes <ohtosttnz@gmail.com>
2022-05-17 20:40:12 -0300

    Sobe código de "conteúdo"

---
+++ 2-front-end/15-gerenciamento-de-estado-com-redux/5-testes-em-react-redux/testando-redux/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import ButtonClicks from './ButtonClicks';
+import NumberClicks from './NumberClicks';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <ButtonClicks />
+          <NumberClicks />
+        </Provider>
+      </div>
+    );
+  }
+}
+export default App;
