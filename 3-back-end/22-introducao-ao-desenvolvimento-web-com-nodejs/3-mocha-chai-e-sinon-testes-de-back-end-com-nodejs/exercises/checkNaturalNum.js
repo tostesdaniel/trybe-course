bca3e54ed57dd275dbe7e2eb571f80577ba18a10
Daniel Tostes <ohtosttnz@gmail.com>
2022-07-29 12:09:46 -0300

    feat: adds treatment when value is not a number

--- 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercises/checkNaturalNum.js
+++ 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercises/checkNaturalNum.js
@@ -1,4 +1,5 @@
 module.exports = (n) => {
+  if (typeof n !== 'number') return 'Error: Value must be a number';
   if (n > 0) return 'positive';
   if (n < 0) return 'negative';
   return 'absolute';
