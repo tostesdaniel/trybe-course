810c88067b2c148b74322b21d5312b5e0e824317
Daniel Tostes <ohtosttnz@gmail.com>
2022-07-30 18:12:02 -0300

    feat: adds function to find character by id

--- 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/2-nodejs-fluxo-assincrono/exercises/simpsons.js
+++ 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/2-nodejs-fluxo-assincrono/exercises/simpsons.js
@@ -1,9 +1,5 @@
 /* Você pode utilizar then/catch ou async/await para escrever seu código. Procure não utilizar callbacks.
 
-    Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome. Por exemplo: 1 - Homer Simpson.
-    // ─────────────────────────────────────────────────────────────────
-    Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
-    // ─────────────────────────────────────────────────────────────────
     Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
     // ─────────────────────────────────────────────────────────────────
     Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json, contendo as personagens com id de 1 a 4.
@@ -12,11 +8,16 @@
     // ────────────────────────────────────────────────────────────────────────────────
     Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json. */
 
-const fs = require('fs').promises;
+// const fs = require('fs/promises');
+const {
+  promises: { readFile },
+} = require('fs');
 
-async function printCharacters() {
+// Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome. Por exemplo: 1 - Homer Simpson.
+
+function printCharacters() {
   try {
-    const data = await fs.readFile('./simpsons.json', { encoding: 'utf8' });
+    const data = fs.readFileSync('./simpsons.json', 'utf8');
     const characters = JSON.parse(data).map(
       ({ id, name }) => `${id} - ${name}`
     );
@@ -26,4 +27,24 @@ async function printCharacters() {
   }
 }
 
-printCharacters();
+// printCharacters();
+
+// ────────────────────────────────────────────────────────────────────────────────
+// Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
+
+async function getCharacter(id) {
+  const data = await readFile('./simpsons.json', 'utf-8');
+  const characters = await JSON.parse(data);
+  const selCharacter = characters.find(
+    (character) => Number(character.id) === id
+  );
+  if (!selCharacter) {
+    console.log('Id não encontrado');
+    return;
+  }
+  console.log(selCharacter);
+}
+
+// ─── Get Bart Simpson Information ───────────────────────────────────────────────
+getCharacter(3);
+getCharacter(0);
