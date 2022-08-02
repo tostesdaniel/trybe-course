1663534d99c38507d19b38943c498b1a4d35ea29
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 11:07:07 -0300

    feat: adds validateWarranty as middleware to POST in /sales

--- 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/5-express-middlewares/exercises/index.js
+++ 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/5-express-middlewares/exercises/index.js
@@ -4,18 +4,30 @@ const port = 3000;
 
 app.use(express.json());
 
-const { validateName, validateInfo, validateDate } = require('./validationMiddleware');
+const {
+  validateName,
+  validateInfo,
+  validateDate,
+  validateWarranty,
+} = require('./validationMiddleware');
 
 /* -------------------------------------------------------------------------- */
 /* 1 - Crie uma rota POST /sales que receba uma requisição                    */
 /*     que envie productName e infos no body da requisição                    */
 /* -------------------------------------------------------------------------- */
 
-app.post('/sales', validateName, validateInfo, validateDate, (req, res) => {
-  return res
-    .status(201)
-    .json({ message: 'Sale has been created successfully' });
-});
+app.post(
+  '/sales',
+  validateName,
+  validateInfo,
+  validateDate,
+  validateWarranty,
+  (_req, res) => {
+    return res
+      .status(201)
+      .json({ message: 'Sale has been created successfully' });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
