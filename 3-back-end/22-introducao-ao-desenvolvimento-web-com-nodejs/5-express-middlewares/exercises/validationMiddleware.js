b9c3e924967d0710b7da0e64743d97be6bee3295
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 11:49:25 -0300

    feat: creates signup validation middleware

--- 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/5-express-middlewares/exercises/validationMiddleware.js
+++ 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/5-express-middlewares/exercises/validationMiddleware.js
@@ -50,4 +50,18 @@ function validateWarranty(req, res, next) {
   next();
 }
 
-module.exports = { validateName, validateInfo, validateDate, validateWarranty };
+function validateSignup(req, res, next) {
+  const { email, password, firstName, phone } = req.body;
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Missing fields' });
+  }
+  next();
+}
+
+module.exports = {
+  validateName,
+  validateInfo,
+  validateDate,
+  validateWarranty,
+  validateSignup,
+};
