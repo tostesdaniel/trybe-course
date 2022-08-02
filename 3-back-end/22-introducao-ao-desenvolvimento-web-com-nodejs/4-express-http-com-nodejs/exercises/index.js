2163dd3e947b2a689bf7b4e8c668b748013cf4a1
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-01 22:03:00 -0300

    feat: adds route POST to /greetings

--- 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/4-express-http-com-nodejs/exercises/index.js
+++ 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/4-express-http-com-nodejs/exercises/index.js
@@ -24,6 +24,21 @@ app.post('/hello', (req, res) => {
   res.status(200).json({ message: `Hello, ${name}!` });
 });
 
+/* -------------------------------------------------------------------------- */
+/* 3 - Crie uma rota POST /greetings                                          */
+/* -------------------------------------------------------------------------- */
+/* Sua rota deve receber o seguinte JSON: { "name": "<nome do usuário>", 
+"age": <idade do usuário> } */
+/* Caso a pessoa usuária tenha idade superior a 17 anos, devolva o JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 - OK */
+/* Caso a pessoa usuária tenha 17 anos ou menos, devolva o JSON { "message": "Unauthorized" } com o status code 401 - Unauthorized */
+
+app.post('/greetings', (req, res) => {
+  const { age, name } = req.body;
+  if (age > Number(17))
+    return res.status(200).json({ message: `Hello, ${name}!` });
+  return res.status(401).json({ message: 'Unauthorized' });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
