291b27dacdf3cea3254c53339ae2670b416ff5bd
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 10:46:20 -0300

    feat: creates infos validation middleware

--- 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/5-express-middlewares/exercises/validationMiddleware.js
+++ 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/5-express-middlewares/exercises/validationMiddleware.js
@@ -6,10 +6,18 @@ function validateName(req, res, next) {
 
   if (productName.length < 4) {
     return res
-    .status(400)
-    .json({ message: 'productName must be at least 4 characters' });
+      .status(400)
+      .json({ message: 'productName must be at least 4 characters' });
   }
   next();
 }
 
-module.exports = validateName;
+function validateInfo(req, res, next) {
+  const { infos } = req.body;
+  if (!infos) return res.status(400).json({ message: 'infos is required' });
+  if (!infos.saleDate || !infos.warrantyPeriod)
+    return res.status(400).json({ message: 'Some infos cannot be blank' });
+  next();
+}
+
+module.exports = { validateName, validateInfo };
