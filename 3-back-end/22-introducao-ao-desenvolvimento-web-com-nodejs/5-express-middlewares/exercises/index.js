692996e57ce394b8855f82a674232074e855ec79
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 11:49:41 -0300

    feat: adds validateSignup as middleware to POST in /signup

--- 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/5-express-middlewares/exercises/index.js
+++ 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/5-express-middlewares/exercises/index.js
@@ -9,6 +9,7 @@ const {
   validateInfo,
   validateDate,
   validateWarranty,
+  validateSignup,
 } = require('./validationMiddleware');
 
 /* -------------------------------------------------------------------------- */
@@ -28,6 +29,18 @@ app.post(
       .json({ message: 'Sale has been created successfully' });
   }
 );
+
+/* -------------------------------------------------------------------------- */
+/* 2 - Crie uma rota POST /signup que receba no body                          */
+/*     da requisição email, password, firstName, phone                        */
+/* -------------------------------------------------------------------------- */
+
+const crypto = require('crypto');
+
+app.post('/signup', validateSignup, (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ token: token });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
