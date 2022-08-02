6aed6dc2d792eb934174dc4a3518222da838d8b4
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-01 21:51:06 -0300

    feat: adds POST route to /hello

--- 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/4-express-http-com-nodejs/exercises/index.js
+++ 3-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/4-express-http-com-nodejs/exercises/index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 /* -------------------------------------------------------------------------- */
-/*                         1 - Crie uma rota GET /ping                        */
+/* 1 - Crie uma rota GET /ping                                                */
 /* -------------------------------------------------------------------------- */
 /* Sua rota deve retornar o seguinte JSON: { message: 'pong' } -------------- */
 
 app.get('/ping', (_req, res) => {
   res.json({ message: 'pong' });
+});
+
+/* -------------------------------------------------------------------------- */
+/* 2 - Crie uma rota POST /hello                                              */
+/* -------------------------------------------------------------------------- */
+/* Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" } */
+/* Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" }. */
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!` });
 });
 
 app.listen(port, () => {
