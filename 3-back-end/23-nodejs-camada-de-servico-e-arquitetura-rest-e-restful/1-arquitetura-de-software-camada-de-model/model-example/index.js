3ec7507dd4503c42eba36c8a5211885aa7efb87c
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-05 17:34:39 -0300

    feat: creates GET /books

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,6 +8,12 @@ app.get('/authors', async (req, res) => {
   const authors = await Author.getAll();
 
   return res.status(200).json(authors);
+});
+
+app.get('/books', (req, res) => {
+  const books = await Book.getAll();
+
+  return res.status(200).json(books);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
