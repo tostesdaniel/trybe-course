d7af12f0c2ce5699730d95573e09c35402f4e60d
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-05 17:16:49 -0300

    feat: moves index.js back to root dir

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const Author = require('./models/Author');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get('/authors', async (req, res) => {
+  const authors = await Author.getAll();
+
+  return res.status(200).json(authors);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
