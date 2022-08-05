fc5ba22fd7f6fb80a34a8562ad088304c4820288
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-05 19:11:01 -0300

    feat: creates GET /authors/:id

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -10,6 +10,16 @@ app.get('/authors', async (req, res) => {
   return res.status(200).json(authors);
 });
 
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
 app.get('/books', async (req, res) => {
   const { author_id } = req.query;
 
