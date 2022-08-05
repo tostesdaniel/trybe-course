cd251c5c83c098be0f4b2788a2daa8cc2279160f
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-05 20:05:47 -0300

    feat: defines isValid method for Author Model

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
@@ -43,6 +43,14 @@ const findById = async (id) => {
   });
 };
 
+const isValid = (firstName, middleName, lastName) => {
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+  if (middleName && typeof middleName !== 'string') return false;
+
+  return true;
+};
+
 module.exports = {
   getAll,
   findById,
