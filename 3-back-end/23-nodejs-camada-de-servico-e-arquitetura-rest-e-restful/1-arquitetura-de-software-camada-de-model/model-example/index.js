862ebca6c163588d167e1cdd70d1b05b68eb5222
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-05 22:56:43 -0300

    feat: creates POST /authors

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const Author = require('./models/Author');
 const Book = require('./models/Book');
+
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get('/authors', async (req, res) => {
   const authors = await Author.getAll();
@@ -38,6 +40,18 @@ app.get('/books/:id', async (req, res) => {
   if (!book) return res.status(404).json({ message: 'Not Found' });
 
   return res.status(200).json(book);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! ' });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
