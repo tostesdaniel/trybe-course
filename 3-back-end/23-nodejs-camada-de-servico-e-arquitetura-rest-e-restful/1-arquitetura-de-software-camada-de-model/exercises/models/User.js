dc247948e676ef210724099059131691879d6369
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-06 13:44:21 -0300

    feat: adds createUser method for user model
    Fixes user validation.

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/exercises/models/User.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/1-arquitetura-de-software-camada-de-model/exercises/models/User.js
@@ -1,15 +1,26 @@
 const pool = require('./connection');
 
 const validateUser = (firstName, lastName, email, password) => {
-  if (!firstName && typeof firstName !== 'string') return false;
-  if (!lastName && typeof lastName !== 'string') return false;
-  if (!email && typeof email !== 'string') return false;
-  if ((!password && typeof password !== 'string') || password.length < 6)
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+  if (!email || typeof email !== 'string') return false;
+  if (!password || typeof password !== 'string' || password.length < 6)
     return false;
 
   return true;
 };
 
+const createUser = async (firstName, lastName, email, password) => {
+  const query =
+    'INSERT INTO model_exercise.users (first_name, last_name, email, password) VALUES (?,?,?,?);';
+  return pool
+    .execute(query, [firstName, lastName, email, password])
+    .then(([{ insertId: id }]) => {
+      return { id, firstName, lastName, email };
+    });
+};
+
 module.exports = {
   validateUser,
+  createUser,
 };
