d9383ccf0dacc70b420b65bdfbe564ea56d3df6e
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-09 16:13:10 -0300

    feat: adds base learning exercise code

---
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/3-arquitetura-web-rest-e-restful/hello-http-methods/index.js
@@ -0,0 +1,40 @@
+const fetch = require('node-fetch');
+
+// Armazenamos o token numa variável.
+// Num ambiente real, esse valor viria do Local Storage, ou de uma variável de ambiente
+const API_TOKEN = '2d635ea9b637ea0f27d58985cc161d64';
+
+// Criamos um novo objeto de Headers
+const headers = new fetch.Headers({
+  Authorization: API_TOKEN,
+});
+
+// Para aquecer, vamos começar com uma requisição do tipo `GET`
+fetch('https://postman-echo.com/get?param1=teste', { headers })
+  .then((response) => {
+    // Ao receber a resposta, verificamos se correu tudo bem
+    if (!response.ok) {
+      // Caso não, forçamos a Promise a ser rejeitada
+      return Promise.reject(response);
+    }
+
+    // Caso esteja tudo OK, lemos o body como JSON
+    return response.json();
+  })
+  .then((data) => {
+    // Por fim, escrevemos o body no console
+    console.log(data);
+  })
+  .catch((errorOrResponse) => {
+    // Em caso de falha simples (a request completou com um status diferente de 2xx)
+    // simplesmente logamos o status no console
+    if (errorOrResponse.status) {
+      return console.error(
+        `Request failed with status ${errorOrResponse.status}`
+      );
+    }
+
+    // Caso tenha acontecido um erro de rede (não foi possível completar a request)
+    // logamos o erro todo
+    console.error(errorOrResponse);
+  });
