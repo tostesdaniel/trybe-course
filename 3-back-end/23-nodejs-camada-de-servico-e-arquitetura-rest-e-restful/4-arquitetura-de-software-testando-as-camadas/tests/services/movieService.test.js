cbc4bcefb2d3c790abd0354926df461d10be1aa5
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-10 15:05:24 -0300

    test: implements failing tests for movie service

---
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/tests/services/movieService.test.js
@@ -0,0 +1,49 @@
+/* eslint-disable mocha/no-mocha-arrows */
+const { expect } = require('chai');
+
+const MoviesService = {
+  create: () => {},
+};
+
+/*
+  Precisamos validar se estamos recebendo todos os campos
+  necessários para a operação. Como trata-se de uma regra
+  de negócio, validaremos na camada de serviços.
+*/
+describe('Insere um novo filme no BD', () => {
+  describe('quando o payload informado não é válido', () => {
+    const payloadMovie = {};
+
+    it('retorna um boolean', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('boolean');
+    });
+
+    it('o boolean contém "false"', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.equal(false);
+    });
+  });
+
+  describe('quando é inserido com sucesso', () => {
+    const payloadMovie = {
+      title: 'Example Movie',
+      directedBy: 'Jane Dow',
+      releaseYear: 1999,
+    };
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui o "id" do novo filme inserido', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.have.a.property('id');
+    });
+  });
+});
