55b56063942948a589ce44aae7a40a07cea6b1e1
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-10 19:09:45 -0300

    chore: creates new dir for exercise

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/src/models/connection.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/src/models/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'notmypassword',
+  password: '04731212',
   database: 'model_example',
 });
 
