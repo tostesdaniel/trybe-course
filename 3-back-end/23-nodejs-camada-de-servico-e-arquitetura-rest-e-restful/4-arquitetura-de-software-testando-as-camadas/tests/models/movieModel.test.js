2063d79607eb05a28df513699c550f211450947b
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-10 12:21:12 -0300

    test: implements failing tests for movie model

---
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/tests/models/movieModel.test.js
@@ -0,0 +1,33 @@
+/* eslint-disable mocha/no-mocha-arrows */
+const { expect } = require('chai');
+
+/*
+  Como ainda não temos a implementação, vamos fixar
+  um objeto simulando os métodos que iremos desenvolver,
+  porém, eles não terão nenhum comportamento
+*/
+const MoviesModel = {
+  create: () => {},
+};
+
+describe('Insere um novo filme no BD', () => {
+  const payloadMovie = {
+    title: 'Example Movie',
+    directedBy: 'Jane Dow',
+    releaseYear: 1999,
+  };
+
+  describe('quando é inserido com sucesso', () => {
+    it('retorna um objeto', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui o "id" do novo filme inserido', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.have.a.property('id');
+    });
+  });
+});
