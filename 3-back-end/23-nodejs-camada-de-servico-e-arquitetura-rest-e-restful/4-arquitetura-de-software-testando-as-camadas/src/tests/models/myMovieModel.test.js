c0ec5ac509456740de3741a814d4b303d7b2ef29
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-10 20:03:46 -0300

    test: implements empty tests for movieModel

---
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/src/tests/models/myMovieModel.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+describe('Ao resgatar um filme do banco de dados', () => {
+  describe('Quando o filme existe', () => {
+    it('Retorna os dados do filme', () => {});
+  });
+
+  describe('Quando o filme não existir', () => {
+    it('Retorna uma mensagem de erro', () => {});
+  });
+});
