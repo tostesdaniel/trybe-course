a5de1f152e25b982d7f6780065b26bf25ee10e8e
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-11 14:10:38 -0300

    fix: wrong function call on tests

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/src/tests/services/movieService.test.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/src/tests/services/movieService.test.js
@@ -67,13 +67,13 @@ describe('Ao resgatar um filme por id do banco de dados', () => {
     after(async () => MoviesModel.getById.restore());
 
     it('Retorna um objeto', async () => {
-      const movie = await MoviesModel.getById(8);
+      const movie = await MoviesService.getById(8);
 
       expect(movie).to.be.an('object');
     });
 
     it('Retorna um objeto com chaves id, title, directedBy e releaseYear', async () => {
-      const movie = await MoviesModel.getById(8);
+      const movie = await MoviesService.getById(8);
 
       expect(movie).to.have.all.keys(
         'id',
@@ -84,7 +84,7 @@ describe('Ao resgatar um filme por id do banco de dados', () => {
     });
 
     it('Retorna um objeto com chaves e valores esperados', async () => {
-      const movie = await MoviesModel.getById(8);
+      const movie = await MoviesService.getById(8);
       
       expect(movie).to.be.equal(MOVIE);
     });
