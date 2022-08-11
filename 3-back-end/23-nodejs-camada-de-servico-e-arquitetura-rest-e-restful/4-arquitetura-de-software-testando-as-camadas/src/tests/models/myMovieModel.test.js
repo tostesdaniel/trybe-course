f4c21d832a10403a593d1ceae384178c7dc95188
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-10 23:39:58 -0300

    test: tests movieModel

--- 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/src/tests/models/myMovieModel.test.js
+++ 3-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/4-arquitetura-de-software-testando-as-camadas/src/tests/models/myMovieModel.test.js
@@ -1,11 +1,54 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
+const connection = require('../../models/connection');
+const movieModel = require('../../models/movieModel');
 
 describe('Ao resgatar um filme do banco de dados', () => {
+  const MOVIE = {
+    id: 8,
+    title: 'Example Movie',
+    directedBy: 'Jane Dow',
+    releaseYear: 1999,
+  };
+
   describe('Quando o filme existe', () => {
-    it('Retorna os dados do filme', () => {});
+    before(async () => sinon.stub(movieModel, 'getById').resolves(MOVIE));
+
+    after(async () => movieModel.getById.restore());
+
+    it('Retorna um objeto', async () => {
+      const movie = await movieModel.getById(8);
+
+      expect(movie).to.be.an('object');
+    });
+
+    it('Retorna um objeto com chaves id, title, directedBy e releaseYear', async () => {
+      const movie = await movieModel.getById(8);
+
+      expect(movie).to.have.all.keys(
+        'id',
+        'title',
+        'directedBy',
+        'releaseYear'
+      );
+    });
+
+    it('Retorna chave', async () => {
+      const movie = await movieModel.getById(8);
+  
+      expect(movie).to.be.equal(MOVIE);
+    });
   });
 
   describe('Quando o filme não existir', () => {
-    it('Retorna uma mensagem de erro', () => {});
+    before(async () => sinon.stub(connection, 'execute').resolves([[]]));
+
+    after(async () => connection.execute.restore());
+
+    it('Retorna null', async () => {
+      const movie = await movieModel.getById(800);
+
+      expect(movie).to.be.equal(null);
+    });
   });
 });
