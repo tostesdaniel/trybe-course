f7ab1858544166ae0c07b765147e7e338fb068b3
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-22 11:58:01 -0300

    feat: add JWT creation

--- 3-back-end/24-nodejs-orm-autenticacao/3-jwt-json-web-token/exercises/hello-jwt/controllers/login.js
+++ 3-back-end/24-nodejs-orm-autenticacao/3-jwt-json-web-token/exercises/hello-jwt/controllers/login.js
@@ -1,7 +1,23 @@
+const jwt = require('jsonwebtoken');
 const validate = require('../validations/login');
 
-module.exports = (req, _res, next) => {
+const { JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const { username } = req.body;
+
   const { error } = validate.login.validate(req.body);
 
   if (error) return next(error);
+
+  const payload = {
+    username,
+    admin: false,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, {
+    expiresIn: '1h',
+  });
+
+  return res.status(200).json({ token });
 };
