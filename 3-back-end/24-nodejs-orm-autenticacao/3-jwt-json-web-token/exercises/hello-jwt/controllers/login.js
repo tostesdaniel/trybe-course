cf17498c95b1400fe5ddb53c7bc8d590826dd08b
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-22 12:04:48 -0300

    feat: add admin validation

--- 3-back-end/24-nodejs-orm-autenticacao/3-jwt-json-web-token/exercises/hello-jwt/controllers/login.js
+++ 3-back-end/24-nodejs-orm-autenticacao/3-jwt-json-web-token/exercises/hello-jwt/controllers/login.js
@@ -4,15 +4,17 @@ const validate = require('../validations/login');
 const { JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { username } = req.body;
+  const { username, password } = req.body;
 
   const { error } = validate.login.validate(req.body);
 
   if (error) return next(error);
 
+  const admin = username === 'admin' && password === 's3nh4S3gur4???';
+
   const payload = {
     username,
-    admin: false,
+    admin,
   };
 
   const token = jwt.sign(payload, JWT_SECRET, {
