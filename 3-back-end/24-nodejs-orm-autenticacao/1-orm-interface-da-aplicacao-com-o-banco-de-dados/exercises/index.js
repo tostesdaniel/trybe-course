bb114442cfdb7e6cbd9a6ab2154dea20adc0c2ad
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-22 18:49:48 -0300

    feat: add GET /books

--- 3-back-end/24-nodejs-orm-autenticacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercises/index.js
+++ 3-back-end/24-nodejs-orm-autenticacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercises/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const Book = require('./controllers/Book');
 
 const app = express();
 const DEFAULT_PORT = 3000;
 const PORT = process.env.PORT || DEFAULT_PORT;
 
 app.use(express.json());
+
+app.get('/books', Book.getAll);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
