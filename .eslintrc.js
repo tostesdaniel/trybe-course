c6579ced0e5778d295127bd03c51148ffd01f1de
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-23 13:11:17 -0300

    chore(deps): initialize eslint with airbnb config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
