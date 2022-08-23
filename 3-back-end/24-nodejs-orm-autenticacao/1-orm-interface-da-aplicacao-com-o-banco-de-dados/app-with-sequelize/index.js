a69cc68c2655e6012238b10e4cba1a7d74299bf9
Daniel Tostes <41529552+tostesdaniel@users.noreply.github.com>
2022-08-23 12:28:30 -0300

    Exercicios/24.1 (#64)
    * chore(deps): eslint rules
    
    * chore(deps): eslint rules
    
    * chore(deps): initialize repository
    
    - install sequelize
    
    * chore(deps): install Sequelize CLI
    
    * chore(deps): install MySQL 2
    
    * chore(deps): initialize Sequelize
    
    * chore: change development database name
    
    * feat: generate user model
    
    * feat: generate user migration
    
    * feat: replace user model Class by Function
    
    - Changes made due to learning purposes
    
    * feat: add email field to migration
    
    * feat: create migration to add phone_num column
    
    * feat: add phone_num to user model
    
    * chore(deps): install express
    
    * feat: add seeds
    
    * feat: add code application example
    
    * feat: alters config structure
    
    * chore(deps): install dotenv
    
    * chore: dotenv example
    
    * chore: update config location
    
    * chore: initialize package
    
    * chore(deps): install express
    
    * chore(deps): install sequelize and mysql2
    
    * chore(deps): install sequelize-cli as devDeps
    
    * feat: initialize sequelize
    
    * chore(deps): install dotenv
    
    * feat: setup config to use dotenv
    
    * chore: create dotenv example
    
    * chore: gitignore .env
    
    * feat: create base express app
    
    * feat: create books migration
    
    * feat: change config file path
    
    * feat: create Book model
    
    * feat: create Book service
    
    * feat: create Book controller
    
    * feat: add GET /books
    
    * chore: add start script
    
    * chore(deps): install nodemon as devDep
    
    * chore: adds debug script
    
    * feat: get book by id service
    
    * feat: get book by id controller
    
    * feat: GET /books/:id
    
    * feat: create book service
    
    * feat: create book controller
    
    * feat: POST /books
    
    * feat: update book service
    
    * feat: update book controller
    
    * feat: PUT /books/:id
    
    * feat: delete book service
    
    * feat: delete book controller
    
    * feat: DELETE /books/:id
    
    * feat: create Books seeder
    
    * feat: get book by author service
    
    * feat: get book by author controller
    
    * feat: GET /books/search
    
    * feat: change getAll behaviour
    
    * feat: getAll and getBuAuthor now returns ordered by title
    
    * refactor: clear code
    
    * feat: create publisher column migration
    
    * feat: add publisher column to model
    
    * feat: add publisher param to functions
    
    * feat: add publisher column to body

---
+++ 3-back-end/24-nodejs-orm-autenticacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const userController = require('./controllers/userController');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/user', userController);
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
---
+++ 3-back-end/24-nodejs-orm-autenticacao/1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercises/controllers/Book.js
@@ -0,0 +1,58 @@
+const Book = require('../services/Book');
+
+module.exports = {
+  getAll: async (req, res) => {
+    const { author } = req.query;
+
+    let books;
+
+    if (author) {
+      books = await Book.getByAuthor(author);
+    } else {
+      books = await Book.getAll();
+    }
+
+    return res.status(200).json(books);
+  },
+  getById: async (req, res) => {
+    const { id } = req.params;
+
+    const book = await Book.getById(id);
+
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+
+    return res.status(200).json(book);
+  },
+  getByAuthor: async (req, res) => {
+    const { author } = req.query;
+
+    const books = await Book.getByAuthor(author);
+
+    return res.status(200).json(books);
+  },
+  create: async (req, res) => {
+    const { title, author, pageQuantity, publisher } = req.body;
+    const book = await Book.create({ title, author, pageQuantity, publisher });
+
+    res.status(201).json(book);
+  },
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { title, author, pageQuantity, publisher } = req.body;
+
+    const book = await Book.update(id, { title, author, pageQuantity, publisher });
+
+    if (!book) return res.status(404).json({ message: 'Book not found!' });
+
+    return res.status(200).json({ message: 'Book updated!' });
+  },
+  delete: async (req, res) => {
+    const { id } = req.params;
+
+    const book = await Book.delete(id);
+
+    if (!book) return res.status(404).json({ message: 'Book not found!' });
+
+    return res.status(200).json({ message: 'Book removed!' });
+  },
+};
