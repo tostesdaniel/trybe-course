e299b03436a4ff72c9f41ffcd47485c32f33435c
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-08 18:18:58 -0300

    feat: create months enum

---
+++ 3-back-end/26-typescript/1-introducao-a-typescript/exercises/my-ts-scripts/exercise2.ts
@@ -0,0 +1,16 @@
+enum Months {
+  Janeiro = 'Janeiro',
+  Fevereiro = 'Fevereiro',
+  Marco = 'Março',
+  Abril = 'Abril',
+  Maio = 'Maio',
+  Junho = 'Junho',
+  Julho = 'Julho',
+  Agosto = 'Agosto',
+  Setembro = 'Setembro',
+  Outubro = 'Outubro',
+  Novembro = 'Novembro',
+  Dezembro = 'Dezembro',
+}
+
+export default Months;
