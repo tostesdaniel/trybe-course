304225b072a4c0dc1797d0aabfa17e9e8692c5a8
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-12 15:19:21 -0300

    feat: add error middleware

--- 3-back-end/26-typescript/3-express-com-typescript/express-typescript/index.ts
+++ 3-back-end/26-typescript/3-express-com-typescript/express-typescript/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const app = express();
@@ -8,6 +8,28 @@ const PORT = 8000;
 
 app.get('/', (_req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('Express + TypeScript');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.log(err);
+      res.sendStatus(500);
+  }
+
+  next();
 });
 
 app.listen(PORT, () => {
