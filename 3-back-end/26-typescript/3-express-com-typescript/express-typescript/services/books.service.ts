688a9a787e8580951e115949bb71e8ef6568ee02
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-12 19:44:45 -0300

    feat: create delete method

--- 3-back-end/26-typescript/3-express-com-typescript/express-typescript/services/books.service.ts
+++ 3-back-end/26-typescript/3-express-com-typescript/express-typescript/services/books.service.ts
@@ -30,6 +30,13 @@ class BookService {
 
     return this.model.update(id, book);
   }
+
+  public async remove(id: number): Promise<void> {
+    const bookFound = await this.model.getById(id);
+    if (!bookFound) throw new NotFoundError('NotFoundError');
+
+    await this.model.remove(id);
+  }
 }
 
 export default BookService;
