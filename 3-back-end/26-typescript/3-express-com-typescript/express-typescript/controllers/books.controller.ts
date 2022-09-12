c6dbf8e65c00708724eeb793c4081940b6b76c59
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-12 16:27:01 -0300

    feat: create getById method

--- 3-back-end/26-typescript/3-express-com-typescript/express-typescript/controllers/books.controller.ts
+++ 3-back-end/26-typescript/3-express-com-typescript/express-typescript/controllers/books.controller.ts
@@ -9,6 +9,16 @@ class BooksController {
     const books = await this.bookService.getAll();
     res.status(StatusCodes.OK).json(books);
   };
+
+  public getById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const book = await this.bookService.getById(id);
+
+    if (!book)
+      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Book not found!' });
+
+    return res.status(StatusCodes.OK).json(book);
+  };
 }
 
 export default BooksController;
