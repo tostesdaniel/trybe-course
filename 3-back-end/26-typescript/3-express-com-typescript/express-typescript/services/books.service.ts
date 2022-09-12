33becae6fc7145ccb88611eb8c224dba31844cfb
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-12 19:38:02 -0300

    feat: create update method

--- 3-back-end/26-typescript/3-express-com-typescript/express-typescript/services/books.service.ts
+++ 3-back-end/26-typescript/3-express-com-typescript/express-typescript/services/books.service.ts
@@ -1,6 +1,7 @@
 import connection from '../models/connection';
 import BookModel from '../models/book.model';
 import Book from '../interfaces/book.interface';
+import { NotFoundError } from 'restify-errors';
 
 class BookService {
   public model: BookModel;
@@ -21,6 +22,13 @@ class BookService {
 
   public async create(book: Book): Promise<Book> {
     return this.model.create(book);
+  }
+
+  public async update(id: number, book: Book): Promise<void> {
+    const bookFound = await this.model.getById(id);
+    if (!bookFound) throw new NotFoundError('NotFoundError');
+
+    return this.model.update(id, book);
   }
 }
 
