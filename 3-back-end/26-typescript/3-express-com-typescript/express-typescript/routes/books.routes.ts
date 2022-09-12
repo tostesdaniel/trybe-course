42be990bc757ec927b0cdd2ac473d5e450c60b02
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-12 16:12:19 -0300

    feat: add GET /books route

--- 3-back-end/26-typescript/3-express-com-typescript/express-typescript/routes/books.routes.ts
+++ 3-back-end/26-typescript/3-express-com-typescript/express-typescript/routes/books.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import BooksController from '../controllers/books.controller';
 
 const router = Router();
+
+const booksController = new BooksController();
+
+router.get('/books', booksController.getAll);
 
 export default router;
