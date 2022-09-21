0f1e756ff537cc0d487c8b55382489bf4a8f6b2b
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-21 13:52:33 -0300

    feat: add new Student

--- 3-back-end/27-poo-e-solid/2-heranca-e-interfaces/exercises/index.ts
+++ 3-back-end/27-poo-e-solid/2-heranca-e-interfaces/exercises/index.ts
@@ -1,9 +1,15 @@
 import Person from './Person';
+import Student from './Student';
 
 const me = new Person('Daniel', new Date('1999/09/17'));
-const definitelyNotMe = new Person('Daniel', new Date('2999/09/17'));
-const notMeEither = new Person('Da', new Date('1500/01/12'));
+// const definitelyNotMe = new Person('Daniel', new Date('2999/09/17'));
+// const notMeEither = new Person('Da', new Date('1500/01/12'));
 
 console.log('me', me);
-console.log('definitelyNotMe', definitelyNotMe);
-console.log('notMeEither', notMeEither);
+// console.log('definitelyNotMe', definitelyNotMe);
+// console.log('notMeEither', notMeEither);
+
+/* Student ------------------------------------------------------------------ */
+const daniel = new Student('Daniel', new Date('1999/09/17'));
+
+console.log('daniel', daniel);
