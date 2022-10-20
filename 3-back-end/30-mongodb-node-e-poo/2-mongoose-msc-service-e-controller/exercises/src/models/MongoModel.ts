82526e1ed3576d7e797ecd639d7ea66827290f84
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-20 16:37:34 -0300

    feat: modify objectId Errors to use ErrorTypes

--- 3-back-end/30-mongodb-node-e-poo/2-mongoose-msc-service-e-controller/exercises/src/models/MongoModel.ts
+++ 3-back-end/30-mongodb-node-e-poo/2-mongoose-msc-service-e-controller/exercises/src/models/MongoModel.ts
@@ -1,5 +1,6 @@
 import { isValidObjectId, Model } from 'mongoose';
 import IModel from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
 
 abstract class MongoModel<T> implements IModel<T> {
   protected _model: Model<T>;
@@ -13,7 +14,7 @@ abstract class MongoModel<T> implements IModel<T> {
   }
 
   public async readOne(_id: string): Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
     return this._model.findOne({ _id });
   }
 
@@ -22,7 +23,7 @@ abstract class MongoModel<T> implements IModel<T> {
   }
 
   public async destroy(_id: string): Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
 
     return this._model.findByIdAndDelete({ _id });
   }
