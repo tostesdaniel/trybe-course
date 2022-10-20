cc49df3bc4f6b6cfe8960b777827b8042d198480
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-20 17:08:58 -0300

    feat: implement Frame Router to app

--- 3-back-end/30-mongodb-node-e-poo/2-mongoose-msc-service-e-controller/exercises/src/app.ts
+++ 3-back-end/30-mongodb-node-e-poo/2-mongoose-msc-service-e-controller/exercises/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error';
+import frameRouter from './routes/frame';
 
 const app = express();
 app.use(express.json());
+app.use(frameRouter);
 app.use(errorHandler);
 
 export default app;
