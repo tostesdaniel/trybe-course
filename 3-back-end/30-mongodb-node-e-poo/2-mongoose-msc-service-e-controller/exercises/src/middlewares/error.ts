e5822402f9b95ac743e198f8b38dc3bafd3b2b43
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-20 16:45:37 -0300

    feat: create error middleware

---
+++ 3-back-end/30-mongodb-node-e-poo/2-mongoose-msc-service-e-controller/exercises/src/middlewares/error.ts
@@ -0,0 +1,26 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import { ErrorTypes, errorCatalog } from '../errors/catalog';
+
+const errorHandler: ErrorRequestHandler = (
+  err: Error | ZodError,
+  _req,
+  res,
+  _next,
+) => {
+  if (err instanceof ZodError) {
+    return res.status(400).json({ message: err.issues });
+  }
+
+  const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+
+  const mappedError = errorCatalog[messageAsErrorType];
+  if (mappedError) {
+    const { httpStatus, message } = mappedError;
+    return res.status(httpStatus).json({ message });
+  }
+  console.error(err);
+  return res.status(500).json({ message: 'internal error' });
+};
+
+export default errorHandler;
