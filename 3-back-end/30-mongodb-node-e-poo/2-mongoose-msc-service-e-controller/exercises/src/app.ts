9f90c41b0b087fa6f4a236604aa452cbfb77dd4b
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-20 16:46:33 -0300

    feat: app now uses error middleware

--- 3-back-end/30-mongodb-node-e-poo/2-mongoose-msc-service-e-controller/exercises/src/app.ts
+++ 3-back-end/30-mongodb-node-e-poo/2-mongoose-msc-service-e-controller/exercises/src/app.ts
@@ -1,7 +1,9 @@
-import express from "express";
-import "express-async-errors";
+import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
 
 const app = express();
 app.use(express.json());
+app.use(errorHandler);
 
 export default app;
